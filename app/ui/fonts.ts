48e3376b6325a77621b8b255120315b25a825111
Sudarshana V Sharma <svsccodes@gmail.com>
2024-06-20 23:29:08 +0530

    first commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Inter,Lusitana } from "next/font/google";
+
+const inter = Inter({subsets:['latin']});
+
+const lusitana = Lusitana({subsets:['latin'],weight:['400','700']})
+
+export {inter,lusitana}
